b623dc5fa43f135d63a06e8ce97607f2920ab48a
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-06-24 20:46:42 +0200

    VIDEO button

--- src/Components/MainContent/Body/Videos/Videos.js
+++ src/Components/MainContent/Body/Videos/Videos.js
@@ -35,7 +35,7 @@ export default function Videos() {
     };
 
     return (
-        <Grid container spacing={2}>
+        <Grid container spacing={2} alignItems="stretch">
             <Grid item xs={12} md={4}>
                 <VideoDescription
                     title={videos[value].title}
